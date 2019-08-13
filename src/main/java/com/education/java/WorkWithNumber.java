8a7ea59d4e746c2326a51cb52088e97b4aa59a69
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-08-13 16:50:53 +0300

    Work with numbers in progress

--- src/main/java/com/education/java/WorkWithNumber.java
+++ src/main/java/com/education/java/WorkWithNumber.java
@@ -2,25 +2,39 @@ package com.education.java;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WorkWithNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] numbers = parse(in.next());
+        String st = in.nextLine();
+        int[] numbers = parse(st);
+       // int[] even=  Even(numbers);
         for (int i = 0; i < numbers.length; i++) {
-
-            System.out.println(numbers[i]);
+            System.out.println( Even(numbers));
         }
     }
 
 
     public static int[] parse(String string) {
-        String[] splited = string.split(" ");
+        String[] splited = string.trim().split("\\s+");
         int[] numbers = new int[splited.length];
         for (int i = 0; i < splited.length; i++) {
             numbers[i] = Integer.parseInt(splited[i]);
         }
         return numbers;
     }
+
+    public static ArrayList<Integer> Even(int[] numbers) {
+        ArrayList<Integer> even = new ArrayList<Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0) {
+                even.add(i);
+            }
+            System.out.println(even[i]);
+        }
+        return even;
+    }
+
 }
