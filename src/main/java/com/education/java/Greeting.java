3ced6bf25d011e7c67c971f73ed4b01fdeb8cfb3
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-08-13 12:08:53 +0300

    Initial commit

---
+++ src/main/java/com/education/java/Greeting.java
@@ -0,0 +1,12 @@
+package com.education.java;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name, please");
+        String name = in.next();
+        System.out.println("Hello " + name +"! "+ "Welcome");
+    }
+}
