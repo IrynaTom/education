31aba66746431262d9cc85b2b62bbd01733c91c8
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-08-13 12:54:12 +0300

    Com line Done

--- src/main/java/com/education/java/ReverseCommandLineArguments.java
+++ src/main/java/com/education/java/ReverseCommandLineArguments.java
@@ -1,13 +1,14 @@
 package com.education.java;
 
-public class Command_line {
+public class ReverseCommandLineArguments {
     public static void main(String[] args) {
         System.out.println(args);
         for(int i=0; i< args.length; i++){
-            System.out.print("First road");
             System.out.print(args[i]+ " ");
-            System.out.print("backward road");
-
+        }
+        System.out.println(" ");
+        for(int i=args.length-1; i>= 0; i--){
+            System.out.print(args[i]+ " ");
         }
     }
 }
