730b199b0b7840fc4dd646415d487a8659c6d905
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-08-13 15:56:46 +0300

    Work with numbers in progress

--- src/main/java/com/education/java/WorkWithNumber.java
+++ src/main/java/com/education/java/WorkWithNumber.java
@@ -1,20 +1,26 @@
 package com.education.java;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WorkWithNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> numbers= new ArrayList<Integer>();
-        while(in.hasNextInt()){
-            numbers.add(in.nextInt());
-        }
-        Integer [] nums = numbers.toArray(new Integer[0]);
-        for(int i = 0; i < nums.length; i++){
-            System.out.println(nums[i]);
+        int[] numbers = parse(in.next());
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.println(numbers[i]);
         }
     }
 
 
+    public static int[] parse(String string) {
+        String[] splited = string.split(" ");
+        int[] numbers = new int[splited.length];
+        for (int i = 0; i < splited.length; i++) {
+            numbers[i] = Integer.parseInt(splited[i]);
+        }
+        return numbers;
+    }
 }
