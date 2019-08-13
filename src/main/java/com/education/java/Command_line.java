b1049560ad0540d22d6c5a34de4bdf84febb07fe
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-08-13 12:38:22 +0300

    Greeting Done Com line start

---
+++ src/main/java/com/education/java/Command_line.java
@@ -0,0 +1,10 @@
+package com.education.java;
+
+public class Command_line {
+    public static void main(String[] args) {
+        System.out.println(args);
+        for(int i=0; i< args.length; i++){
+            System.out.println(i);
+        }
+    }
+}
